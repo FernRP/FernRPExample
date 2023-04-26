0c324175864a8e46696b4ddbdc8a280b6f415741
gifplane <itact@hotmail.com>
2023-04-26 09:27:13 +0800

    improve Rendersetting Prop
    1. add depth prepass (For Test)
    2. add cast shadow control
    3. subEnum can get enum type now

--- Assets/FernNPRCore/Shader/NPR Standard/NPRStandardInput.hlsl
+++ Assets/FernNPRCore/Shader/NPR Standard/NPRStandardInput.hlsl
@@ -121,6 +121,10 @@ half _Cutoff;
 half _Surface;
 half _ClipThresold;
 
+// AI
+half _Is_SDInPaint;
+half _ClearShading;
+
 half _OutlineWidth;
 CBUFFER_END
 
@@ -324,6 +328,7 @@ inline void InitializeNPRStandardSurfaceData(float2 uv, InputData inputData, out
     #endif
     uv += uvOffset;
     half4 albedoAlpha = SampleAlbedoAlpha(uv, TEXTURE2D_ARGS(_BaseMap, sampler_BaseMap));
+    albedoAlpha.rgb = lerp(albedoAlpha.rgb, 1, _ClearShading);
     half4 pbrLightMap = SAMPLE_TEXTURE2D(_LightMap, sampler_LightMap, uv);
     half4 pbrChannel = SamplePBRChannel(pbrLightMap, shadingMap01);
     outSurfaceData.alpha = Alpha(albedoAlpha.a, _BaseColor, _Cutoff);
@@ -340,8 +345,6 @@ inline void InitializeNPRStandardSurfaceData(float2 uv, InputData inputData, out
     outSurfaceData.specularIntensity = 1;
     #endif
     outSurfaceData.emission = EmissionColor(pbrLightMap, shadingMap01, outSurfaceData.albedo, uv);
-
-   
 }
 
 
