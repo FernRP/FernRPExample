921cc7ac1d91a23b09791ec8031e85b804618c8c
Dongjun Huang (黄东俊) <dongjun.huang@unity.cn>
2023-04-29 10:25:32 +0800

    update specularAA

--- Assets/FernNPRCore/Shader/NPR Standard/NPRStandardInput.hlsl
+++ Assets/FernNPRCore/Shader/NPR Standard/NPRStandardInput.hlsl
@@ -319,11 +319,10 @@ inline half SpecularAA(half3 normalWS, half smoothness)
     half dx = dot(ddx(normalWS),ddx(normalWS));
     half dy = dot(ddy(normalWS),ddy(normalWS));
     half roughness = 1 - smoothness;
-    half roughnessAA = roughness * roughness + min(_SpaceScreenVariant * (dx + dy) * 2, _SpecularAAThreshold * _SpecularAAThreshold);
+    half roughnessAA = roughness * roughness + min(_SpaceScreenVariant * (dx + dy) * 8, _SpecularAAThreshold * _SpecularAAThreshold);
     roughnessAA = saturate(roughnessAA);
-    roughnessAA = sqrt(roughnessAA);
-    roughnessAA = sqrt(roughnessAA);
-    half smoothnessAA = 1 - roughnessAA;
+    //roughnessAA = sqrt(roughnessAA);
+    half smoothnessAA = smoothness * (1 - roughnessAA);
     return smoothnessAA;
 }
 
